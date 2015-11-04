87cb19dc238acebf26bf3065960d24e2e1e06a19
Mark Wunsch <mwunsch@renttherunway.com>
2015-11-03 19:43:43 -0500

    Pull in Overtone dep

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject sonic-sketches "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Studies in Clojure and Overtone"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [overtone "0.9.1"]]
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
