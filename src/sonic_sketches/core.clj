b9c3989517ef3e756909221771dfcc2a7a5c1a84
Mark Wunsch <mwunsch@renttherunway.com>
2015-11-11 16:45:02 -0500

    Remove shutdown hook. TURNS OUT Overtone does this

--- src/sonic_sketches/core.clj
+++ src/sonic_sketches/core.clj
@@ -2,13 +2,10 @@
   (:use [overtone.live])
   (:gen-class))
 
-
-
 (definst mousedrums []
   (example membrane-circle :mouse))
 
 (defn -main
   "I like to play the drums by clicking my mouse on the screen."
   [& args]
-  (.addShutdownHook (Runtime/getRuntime) (Thread. #(stop)))
   (mousedrums))
