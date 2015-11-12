f1a7ea0be782af0288ca6f236fee5f88e193a815
Mark Wunsch <mwunsch@renttherunway.com>
2015-11-12 17:13:34 -0500

    Remove beckon dependency

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject sonic-sketches "0.1.0-SNAPSHOT"
   :description "Studies in Clojure and Overtone"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [overtone "0.9.1"]
-                 [beckon "0.1.1"]]
+                 [overtone "0.9.1"]]
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
