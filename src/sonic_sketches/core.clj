df0a3961a0e3c91eb0808af0083ed774ee1f6e9f
Mark Wunsch <mwunsch@renttherunway.com>
2015-11-25 12:20:59 -0500

    Record sound to filesystem. This isn't right.

--- src/sonic_sketches/core.clj
+++ src/sonic_sketches/core.clj
@@ -8,8 +8,10 @@
 (defn play-then-quit
   "Demo synth for n millis, then exit"
   [t ugen-fn]
+  (recording-start "./sounds/test.wav")
   (ugen-fn)
-  (after-delay t #(System/exit 0)))
+  (after-delay t (fn [] ((do (recording-stop)
+                            (System/exit 0))))))
 
 (defn -main
   "I like to play the drums by clicking my mouse on the screen."
