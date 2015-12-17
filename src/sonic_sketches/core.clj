1ea8231bddbcd12f0cd51c186bcb9ff6976543be
Mark Wunsch <mwunsch@renttherunway.com>
2015-12-17 12:08:37 -0500

    Pull in Monotron example, play random notes

--- src/sonic_sketches/core.clj
+++ src/sonic_sketches/core.clj
@@ -2,16 +2,49 @@
   (:use [overtone.live])
   (:gen-class))
 
-(definst mousedrums []
-  (example membrane-circle :mouse))
+;; ======================================================================
+;; Monotron Clone by Roger Allen.
+;;   via some code in https://github.com/rogerallen/explore_overtone
+;;
+;; Source
+;; http://korg.com/monotrons
+;; http://korg.com/services/products/monotron/monotron_Block_diagram.jpg
+;;
+;; Following patterns from
+;; https://github.com/overtone/overtone/blob/master/src/overtone/inst/synth.clj
+;;
+;; Inspiration
+;; http://www.soundonsound.com/sos/aug10/articles/korg-monotron.htm
+;; http://www.timstinchcombe.co.uk/index.php?pge=mono
+;;
+;; This was pulled from https://github.com/overtone/overtone/blob/master/src/overtone/examples/instruments/monotron.clj
+(defsynth monotron
+  "Korg Monotron from website diagram:
+   http://korg.com/services/products/monotron/monotron_Block_diagram.jpg."
+  [note     60            ; midi note value
+   volume   0.7           ; gain of the output
+   mod_pitch_not_cutoff 1 ; use 0 or 1 only to select LFO pitch or cutoff modification
+   pitch    0.0           ; frequency of the VCO
+   rate     4.0           ; frequency of the LFO
+   int      1.0           ; intensity of the LFO
+   cutoff   1000.0        ; cutoff frequency of the VCF
+   peak     0.5           ; VCF peak control (resonance)
+   pan      0             ; stereo panning
+   ]
+  (let [note_freq       (midicps note)
+        pitch_mod_coef  mod_pitch_not_cutoff
+        cutoff_mod_coef (- 1 mod_pitch_not_cutoff)
+        LFO             (* int (saw rate))
+        VCO             (saw (+ note_freq pitch (* pitch_mod_coef LFO)))
+        vcf_freq        (+ cutoff (* cutoff_mod_coef LFO) note_freq)
+        VCF             (moog-ff VCO vcf_freq peak)
+        ]
+    (out 0 (pan2 (* volume VCF) pan))))
 
-(def kick (sample (freesound-path 2086)))
-
-(defn play-bars [bar-num nome sound]
-  (when (not (zero? bar-num))
-    (let [beat (nome)]
-      (at (nome beat) (sound))
-      (apply-by (nome (inc beat)) #'play-bars [(dec bar-num) nome sound]))))
+(defn play-monotron [tron nome note]
+  (let [beat (nome)]
+    (at (nome beat) (ctl tron :note note))
+    (apply-by (nome (inc beat)) #'play-monotron [tron nome (choose (scale :C4 :major))])))
 
 (defn play
   "Record a fn that starts a synth-node and call a callback when that node is destroyed."
