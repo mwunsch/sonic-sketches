7cdb4864e07cf2c171d05f748481be11b65104a7
Mark Wunsch <mwunsch@renttherunway.com>
2016-01-06 12:18:02 -0500

    Pull in core async dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject sonic-sketches "0.1.0-SNAPSHOT"
   :description "Studies in Clojure and Overtone"
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [overtone "0.9.1"]]
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
