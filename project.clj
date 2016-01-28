f76a74fdfb23b4d24390e4499f2618cc8e79dbbb
Mark Wunsch <mwunsch@renttherunway.com>
2016-01-28 17:00:50 -0500

    Pull in Forecast Clojure API dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [overtone "0.9.1"]
-                 [amazonica "0.3.47"]]
+                 [amazonica "0.3.47"]
+                 [forecast-clojure "1.0.3"]]
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
