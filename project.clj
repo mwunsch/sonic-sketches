4ef1a585fe3f257304fe5ef579f4a8ca0eec218f
Mark Wunsch <mwunsch@renttherunway.com>
2016-02-01 17:10:07 -0500

    Remove forecast.io client and upgrade amazonica
    Getting some strange errors in lein and want to isolate where they're
    coming from

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [overtone "0.9.1"]
-                 [amazonica "0.3.47"]
-                 [forecast-clojure "1.0.3"]]
+                 [amazonica "0.3.49"]]
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
