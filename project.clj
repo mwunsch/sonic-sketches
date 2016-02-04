9b662157c066daaa83315d6713b3dbe47d580ddf
Mark Wunsch <mwunsch@renttherunway.com>
2016-02-04 16:49:13 -0500

    Pull in data generators for RNG

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Studies in Clojure and Overtone"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
+                 [org.clojure/data.generators "0.1.2"]
                  [overtone "0.9.1"]
                  [amazonica "0.3.49"]]
   :main ^:skip-aot sonic-sketches.core
