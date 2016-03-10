645aed3e3c40f7663932424b1f9dd8ebaedb530f
Mark Wunsch <mwunsch@renttherunway.com>
2016-03-10 13:50:33 -0500

    Simplify forecast `call` fn and compose call for NYC

--- src/sonic_sketches/forecast.clj
+++ src/sonic_sketches/forecast.clj
@@ -7,20 +7,26 @@
 (def api-key
   (System/getenv "FORECAST_API_KEY"))
 
-(def nyc-geo
-  "The latitude and longitude of Grand Central Terminal"
-  [40.7528 -73.9765])
-
 (defn call
   "Performs a call to the Forecast API for a daily summary. Accepts a
   pair of lat/long coords and a Long time in millis (like that found
   from System/currentTimeMillis). Will throw an Exception if
   unsuccessful."
-  [coords time]
+  [lat lon time]
   (let [base-url "https://api.forecast.io/forecast"
-        [lat lon] coords
         unix-time (quot time 1000)
         path (join "," (map str [lat lon unix-time]))
         url (join "/" [base-url api-key path])]
     (-> (http/get url {:query-params {:exclude "minutely,hourly,currently"}})
         (update :body json/read-str :key-fn keyword))))
+
+(def nyc-geo
+  "The latitude and longitude of Grand Central Terminal"
+  [40.7528 -73.9765])
+
+(def nyc-at
+  "A partial function that when given a time in millis will call the
+  Forecast API and get a daily weather summary for New York City.
+
+    e.g. (forecast/nyc-at (now))"
+  (apply partial call nyc-geo))
