5b2cfd7ad911703c0f7fe5dd49f2422ab17b6df8
Mark Wunsch <mwunsch@renttherunway.com>
2016-03-10 13:41:11 -0500

    Refine call to Forecast API for proper data
    Parse the body as JSON and call the time-sensitive endpoint

--- src/sonic_sketches/forecast.clj
+++ src/sonic_sketches/forecast.clj
@@ -8,10 +8,19 @@
   (System/getenv "FORECAST_API_KEY"))
 
 (def nyc-geo
-  [40.7903 73.9597])
+  "The latitude and longitude of Grand Central Terminal"
+  [40.7528 -73.9765])
 
 (defn call
-  [coords & params]
+  "Performs a call to the Forecast API for a daily summary. Accepts a
+  pair of lat/long coords and a Long time in millis (like that found
+  from System/currentTimeMillis). Will throw an Exception if
+  unsuccessful."
+  [coords time]
   (let [base-url "https://api.forecast.io/forecast"
-        url (join "/" [base-url api-key (join "," (map str coords))])]
-    (http/get url)))
+        [lat lon] coords
+        unix-time (quot time 1000)
+        path (join "," (map str [lat lon unix-time]))
+        url (join "/" [base-url api-key path])]
+    (-> (http/get url {:query-params {:exclude "minutely,hourly,currently"}})
+        (update :body json/read-str :key-fn keyword))))
