fdf32bf49a13be217eaace3f31d2368807b89be0
Mark Wunsch <mwunsch@renttherunway.com>
2016-04-20 11:02:43 -0400

    Upgrade amazonica

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/data.generators "0.1.2"]
                  [overtone "0.9.1"]
-                 [amazonica "0.3.49"]
+                 [amazonica "0.3.55"]
                  [clj-http "2.1.0"]]
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
