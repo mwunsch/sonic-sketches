54a7c124a088d1ca651cc506aad0ffc2050c42cf
Mark Wunsch <mark@markwunsch.com>
2016-07-27 11:32:12 -0400

    Update project version to 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sonic-sketches "0.5.0-SNAPSHOT"
+(defproject sonic-sketches "0.6.0-SNAPSHOT"
   :description "Studies in Clojure and Overtone"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
