265b61bd7c554cdb8417291e158a938cbd790497
Mark Wunsch <mark@markwunsch.com>
2016-07-28 10:33:35 -0400

    Remove cljs - wont be using it

--- project.clj
+++ project.clj
@@ -3,14 +3,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/data.generators "0.1.2"]
-                 [org.clojure/clojurescript "1.8.51"]
                  [overtone "0.9.1"]
                  [amazonica "0.3.55"]
                  [clj-http "2.1.0"]
                  [com.taoensso/timbre "4.3.1"]]
-  :plugins [[lein-cljsbuild "1.1.3"]]
-  :cljsbuild {
-    :builds [{:source-paths ["src/cljs"]}]}
   :main ^:skip-aot sonic-sketches.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
