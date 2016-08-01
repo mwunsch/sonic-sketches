454399f0cf42c92b46e79be0c81bd25ab667f147
Mark Wunsch <mark@markwunsch.com>
2016-08-01 10:13:33 -0400

    Update tweet with message and geolocation

--- src/sonic_sketches/twitter.clj
+++ src/sonic_sketches/twitter.clj
@@ -1,6 +1,8 @@
 (ns sonic-sketches.twitter
   "Utilities for uploading a song to Twitter"
-  (:use [clojure.java.shell :only [sh]])
+  (:use [clojure.java.shell :only [sh]]
+        [sonic-sketches.core :only [lunar-str]]
+        [clojure.string :only [capitalize]])
   (:import [twitter4j TwitterFactory StatusUpdate GeoLocation]))
 
 (defonce client (TwitterFactory/getSingleton))
@@ -10,7 +12,20 @@
   (let [out "test.mp4"]
     (sh "ffmpeg" "-i" path "-strict" "experimental" out)))
 
+(defn mkstatus
+  [songdata]
+  (let [{:keys [day-of-week iso8601 lunar-phase avg-temp precipitation]} songdata
+        moonphase (lunar-str lunar-phase)
+        day (capitalize day-of-week)
+        message (format "It is %s." day)]
+    (str message "\n\n" moonphase)))
+
 (defn tweet
   "Tweet the song from a path"
   [path metadata]
-  (.updateStatus client "Hello, world."))
+  (let [{:keys [latitude longitude]} metadata
+        statusmsg (mkstatus metadata)
+        geo (GeoLocation. latitude longitude)
+        status (doto (StatusUpdate. statusmsg)
+                 (.setLocation geo))]
+    (.updateStatus client status)))
